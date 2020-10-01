e65fac9e91b3bc959d28f100368598e910043aa1
AMADI AUSTIN CHUKWUEMEKA <38590494+rexsteroxy@users.noreply.github.com>
2020-10-01 11:33:18 +0100

    Update app.php

--- config/app.php
+++ config/app.php
@@ -162,7 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
-        Barryvdh\Debugbar\ServiceProvider::class,
+       
         /*
          * Package Service Providers...
          */
